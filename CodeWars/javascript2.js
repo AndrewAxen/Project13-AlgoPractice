8f09753360aa0232b4abf3c42f31113f09e44ae4
Andrew Axentev <content.manager.site.ru@gmail.com>
2023-10-24 11:50:43 -0400

    2

---
+++ CodeWars/javascript2.js
@@ -0,0 +1,33 @@
+/*
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+*/
+
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+  }
+  
+  const name = "John"; // Replace "John" with the name you want to greet
+  const greeting = greet(name);
+  console.log(greeting);
+
+
+  /* OR */
+  
+  function generateGreeting(name2) {
+    return `Hello, ${name2} how are you doing today?`;
+  }
+  
+  let inputName = prompt("Please enter your name:");
+  
+  // Continue to prompt the user until a non-empty name is provided
+  while (inputName === null || inputName.trim() === "") {
+    inputName = prompt("Please enter a valid name. Your name cannot be empty: ");
+  }
+  
+  const greeting2 = generateGreeting(inputName);
+  console.log(greeting2);
+  
